53358fa93b5949ff327a05339d178d18cb05e639
wangyouzheng <353735348@qq.com>
2023-05-05 15:17:33 +0800

    finished

--- src/3- React应用（脚手架）/3- 组件编码/src/components/Footer/index.js
+++ src/3- React应用（脚手架）/3- 组件编码/src/components/Footer/index.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import './index.css';
 
 class Index extends Component {
-
     /**
      * 设置全选
      *
@@ -28,10 +27,9 @@ class Index extends Component {
 
         return (
             <div className="todo-footer">
-                <input type="checkbox" onChange={this.handleCheckAll}/>
-                <span>
-                        <span>已完成{selected}/全部{total}</span>
-                    </span>
+                <input id='checkAll' type="checkbox" onChange={this.handleCheckAll} checked={total && selected === total ? true : false}/>
+                <label htmlFor='checkAll'>已完成{selected}/全部{total}</label>
+
 
                 <button onClick={this.handleClearAllDone} className="btn btn-danger">清除已完成任务</button>
             </div>
