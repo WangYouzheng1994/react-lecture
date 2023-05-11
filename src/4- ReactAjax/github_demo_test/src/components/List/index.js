16df9518e02975f4884c26d8f3b94907529f9d78
wangyouzheng <353735348@qq.com>
2023-05-11 16:18:19 +0800

    完成list组件的布局，以及查询的回显

--- src/4- ReactAjax/github_demo_test/src/components/List/index.js
+++ src/4- ReactAjax/github_demo_test/src/components/List/index.js
@@ -4,16 +4,22 @@ class List extends Component {
 
     render() {
         return (
-            <div className="row mt-3 border">
-                <div className="align-items-start">
-                    <img className="img-thumbnail" style={{width: "100px", height: "100px"}}
-                         src="https://hbimg.huaban.com/871aa2b9b0a55be3f0738d76a15e23ebac5c586811e29-B6Aozr_fw658"/>
-                    <img className="img-thumbnail" style={{width: "100px", height: "100px"}}
-                         src="https://hbimg.huaban.com/871aa2b9b0a55be3f0738d76a15e23ebac5c586811e29-B6Aozr_fw658"/>
-                    <img className="img-thumbnail" style={{width: "100px", height: "100px"}}
-
-                         src="https://hbimg.huaban.com/871aa2b9b0a55be3f0738d76a15e23ebac5c586811e29-B6Aozr_fw658"/>
-                </div>
+            <div className="row row-cols-lg-4 row-cols-md-2 g-4 mt-1 ">
+                {
+                    this.props.users.map((user, index) => {
+                        return (
+                            <div className="card text-center ">
+                                <a className="card-link " href={user.html_url}>
+                                    <img className="img-thumbnail " style={{"width": "100px"}} alt="avater"
+                                         src={user.avatar_url}/>
+                                </a>
+                                <div className="card-body">
+                                    <p className="card-text">{user.login}</p>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
             </div>
         )
     }
