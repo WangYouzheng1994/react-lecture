2f660dacd952a19c0930d172ed82c946f770f429
wangyouzheng <353735348@qq.com>
2023-05-11 16:25:00 +0800

    传递公用状态给List

--- src/4- ReactAjax/github_demo_test/src/App.js
+++ src/4- ReactAjax/github_demo_test/src/App.js
@@ -6,17 +6,31 @@ import List from './components/List'
 
 
 class App extends React.Component {
-    state = {users:[]}
+    state = {users:[], isFirst: true, isLoading: false}
 
+    /**
+     * 保存用户列表
+     *
+     * @param users
+     */
     saveUsers = (users) => {
         this.setState({users: users})
+    }
+
+    /**
+     * 刷新状态
+     *
+     * @param state
+     */
+    updateAppState = (state) => {
+        this.setState(state);
     }
 
     render() {
         return (
             <div className="container">
-                <Header saveUsers = {this.saveUsers}></Header>
-                <List users={this.state.users}></List>
+                <Header updateAppState={this.updateAppState} saveUsers = {this.saveUsers}></Header>
+                <List users={this.state.users} isLoading={this.state.isLoading}></List>
             </div>
         )
     }
