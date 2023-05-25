90a4a3d152a001ff9a7a798fd22f330e6403e8e4
wangyouzheng <353735348@qq.com>
2023-05-25 18:42:25 +0800

    rename

--- src/5- React路由/1-react_router_lecture/src/App.js
+++ src/5- React路由/1-react_router_lecture/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter,HashRouter, Link, Route, Routes} from 'react-router-dom'
+import {BrowserRouter,HashRouter, Link, NavLink, Route, Routes} from 'react-router-dom'
 
 import About from './component/About'
 import Home from './component/Home'
@@ -18,17 +18,20 @@ class App extends React.Component {
                     <div className="row ">
                         <div className="col-md-2">
                             <ul className="list-group">
-                                {/* 原生的页面是通过a标签的url进行跳转的，他不是一种局部的刷新 */}
+                                {/* 原生的页面是通过a标签的url进行跳转的，他不是一种局部的刷新
                                 <li className="list-group-item active" aria-current="true"><a
                                     href="base_static_about.html">about</a></li>
                                 <li className="list-group-item"><a href="base_static_home.html">home</a></li>
                                 <li className="list-group-item">A third item</li>
                                 <li className="list-group-item">A fourth item</li>
-                                <li className="list-group-item">And a fifth one</li>
+                                <li className="list-group-item">And a fifth one</li>*/}
                                 {/*在React中靠路由链接实现切换组件--编写路由的链接，相当于导航*/}
-
                                 <Link className="list-group-item" to="/about">About</Link>
-                                <Link className="list-group-item" to="/home">Home</Link>
+                                {/*NavLInk 在选中的时候 会增加一个active的class，正好和bootstrap迎合了~*/}
+                                <NavLink className="list-group-item" to="/home">Home</NavLink>
+                                <NavLink activeClassName="aaaaa" className="list-group-item" to="/about">测试Navlink的自定义className, 5.0版本</NavLink>
+                                {/*https://reactrouter.com/en/main/components/nav-link  需要提供一个 function*/}
+                                <NavLink className="aaaaa" className="list-group-item" to="/about">测试Navlink的自定义className, 6.0版本</NavLink>
                             </ul>
                         </div>
 
@@ -41,7 +44,7 @@ class App extends React.Component {
                                         {/*<Route path="/about" component={About}></Route>
                                         <Route path="/home" component={Home}></Route>*/}
                                         {/* 6.x   */}
-                                        <Route path="/about" element={<About/>}></Route>
+                                        <Route path="/about" element={<About abc={"abc"}/>}></Route>
                                         <Route path="/home" element={<Home/>}></Route>
                                     </Routes>
                                 </div>
@@ -54,20 +57,10 @@ class App extends React.Component {
                     <div className="row ">
                         <div className="col-md-2">
                             <ul className="list-group">
-                                {/* 原生的页面是通过a标签的url进行跳转的，他不是一种局部的刷新 */}
-                                <li className="list-group-item active" aria-current="true"><a
-                                    href="base_static_about.html">about</a></li>
-                                <li className="list-group-item"><a href="base_static_home.html">home</a></li>
-                                <li className="list-group-item">A third item</li>
-                                <li className="list-group-item">A fourth item</li>
-                                <li className="list-group-item">And a fifth one</li>
-                                {/*在React中靠路由链接实现切换组件--编写路由的链接，相当于导航*/}
-
                                 <Link className="list-group-item" to="/about">About</Link>
                                 <Link className="list-group-item" to="/home">Home</Link>
                             </ul>
                         </div>
-
                         <div className="col-md-10">
                             <div className="panel">
                                 {/* 注册路由的 展示区，path对应了link的to，component对应了路由的组件 */}
