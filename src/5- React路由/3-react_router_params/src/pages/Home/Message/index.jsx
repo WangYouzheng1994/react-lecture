c9e4bb4fb45605eb64d8f9e1f510844b021f4448
wangyouzheng <353735348@qq.com>
2023-06-14 13:15:20 +0800

    message ul li 使用数组进行动态渲染

--- src/5- React路由/3-react_router_params/src/pages/Home/Message/index.jsx
+++ src/5- React路由/3-react_router_params/src/pages/Home/Message/index.jsx
@@ -1,20 +1,41 @@
 import React, {Component} from 'react'
+import Detail from './Detail'
 
 export default class Message extends Component {
+    state = {
+        messageArr: [
+            {id:'01', title: '消息1'},
+            {id:'02', title: '消息2'},
+            {id:'03', title: '消息5555'},
+        ]
+    }
     render() {
+        let {messageArr} = this.state;
         return (
             <div>
                 <ul>
-                    <li>
-                        <a>message001</a>
-                    </li>
-                    <li>
-                        <a>message002</a>
-                    </li>
-                    <li>
-                        <a>message003</a>
-                    </li>
+                    {
+                        /*<li>
+                            <a>message001</a>
+                        </li>
+                        <li>
+                            <a>message002</a>
+                        </li>
+                        <li>
+                            <a>message003</a>
+                        </li>*/
+                        messageArr.map((msgObj)=>{
+                            return (
+                                <li key={msgObj.id}>
+                                <a>{msgObj.title}</a>
+                                </li>
+                            );
+                        })
+                    }
                 </ul>
+                {/*切记，如果要写js代码 必须要用花括号括起来 <ul><li>{messageArr.length}</li></ul>*/}
+                <hr/>
+                <Detail/>
             </div>
         )
     }
