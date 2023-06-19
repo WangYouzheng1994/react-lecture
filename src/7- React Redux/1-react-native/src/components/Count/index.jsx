460dda10b4b2ebc5ab4b6d6cf7eec327578784ff
wangyouzheng <353735348@qq.com>
2023-06-19 14:20:52 +0800

    原生 实现自动加和

--- src/7- React Redux/1-react-native/src/components/Count/index.jsx
+++ src/7- React Redux/1-react-native/src/components/Count/index.jsx
@@ -1,20 +1,49 @@
 import React, {Component} from 'react'
 
 export default class Count extends Component {
+    state = {count:0}
+
+    increment = () => {
+        const {value} = this.selectNumber;
+        const {count} = this.state;
+        // value*1的目的是转成数字 否则count+value变成拼串了
+        this.setState({count: count+value*1})
+    }
+    decrement = () => {
+        const {value} = this.selectNumber;
+        const {count} = this.state;
+        this.setState({count: count-value*1})
+    }
+    incrementIfOdd = () => {
+        const {value} = this.selectNumber;
+        const {count} = this.state;
+        if (count %2 !== 0) {
+            this.setState({count: count+value*1})
+        }
+    }
+    incrementAsync = () => {
+        const {value} = this.selectNumber;
+        const {count} = this.state;
+        setTimeout(() => {
+            this.setState({count: count+value*1})
+        }, 500)
+    }
+
     render() {
         return (
             <div>
-                <h1>当前求和为：0</h1>
+                <h1>当前求和为：{this.state.count}</h1>
+                {/*把select节点存到了 this.selectNumber*/}
                 <select ref={c => this.selectNumber = c}>
                     <option value="1">1</option>
                     <option value="2">2</option>
                     <option value="3">3</option>
                 </select>
 
-                <button>+</button>
-                <button>-</button>
-                <button>当前求和为奇数再加</button>
-                <button>异步加和</button>
+                <button onClick={this.increment}>+</button>
+                <button onClick={this.decrement}>-</button>
+                <button onClick={this.incrementIfOdd}>当前求和为奇数再加</button>
+                <button onClick={this.incrementAsync}>异步加和</button>
             </div>
         )
     }
--- src/7- React Redux/readme.md
+++ src/7- React Redux/readme.md
@@ -1,1 +1,14 @@
+Redux中文入门教程链接
 https://www.redux.org.cn/docs/introduction/PriorArt.html
+
+---
+### Redux三大核心概念
+1. action
+* 包含俩属性：type 唯一字符串，相当于接口，data 数据
+```
+{type:"add_student", data:{name:"zhangsan", age:25}}
+```
+2. reducer
+* 他根据state+action，负责初始化以及加工store的数据，
+3. store
+* 负责存储数据，连接了action和reducer
