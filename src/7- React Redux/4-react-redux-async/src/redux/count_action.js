7fa1a44067b137541ef2080f4c5e7cf601bd9112
wangyouzheng <353735348@qq.com>
2023-06-20 15:21:53 +0800

    redux 完整版 redux-thunk + 非redux-thunk

---
+++ src/7- React Redux/4-react-redux-async/src/redux/count_action.js
@@ -0,0 +1,35 @@
+import {INCREMENT, DECREMENT} from "./constant";
+import store from './store'
+
+/**
+ * 该文件用于Count组件生成action对象
+ */
+export const createIncrementAction = data => ({type: INCREMENT, data})
+export const createDecrementAction = data => ({type: DECREMENT, data})
+/* 用lambda进行简化
+function createDecrementAction(data) {
+    return {type:'decrement', data};
+}
+*/
+
+/**
+ * 异步的action
+ */
+export const createIncrementAsyncAction = (data, time) => {
+    /*
+        自己要调用store*/
+    return () => {
+        setTimeout(() => {
+           store.dispatch(createIncrementAction(data))
+        }, time);
+    }
+
+    /**
+     * 直接用redux傳進來的dispatch實現
+     */
+    /*return (dispatch) => {
+        setTimeout(() => {
+            dispatch(createIncrementAction(data))
+        }, time);
+    }*/
+}
