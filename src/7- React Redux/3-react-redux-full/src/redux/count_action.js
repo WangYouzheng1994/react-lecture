42e556096a644e6dec6311b676243ae7317b9327
wangyouzheng <353735348@qq.com>
2023-06-20 11:45:17 +0800

    redux 完整版。

--- src/7- React Redux/3-react-redux-full/src/redux/count_action.js
+++ src/7- React Redux/3-react-redux-full/src/redux/count_action.js
@@ -1,8 +1,10 @@
+import {INCREMENT, DECREMENT} from "./constant";
+
 /**
  * 该文件用于Count组件生成action对象
  */
-export const createIncrementAction = data => ({type: 'increment', data})
-export const createDecrementAction = data => ({type: 'decrement', data})
+export const createIncrementAction = data => ({type: INCREMENT, data})
+export const createDecrementAction = data => ({type: DECREMENT, data})
 /* 用lambda进行简化
 function createDecrementAction(data) {
     return {type:'decrement', data};
