61d65d2ba1e3c1b7b01bf18077f786389199a342
wangyouzheng <353735348@qq.com>
2023-06-25 18:34:01 +0800

    redux person和countreducer进行合并

--- src/7- React Redux/8-react-redex-components-datachange/src/redux/store.js
+++ src/7- React Redux/8-react-redex-components-datachange/src/redux/store.js
@@ -1,12 +1,18 @@
 /**
  * 该文件用于暴露一个 store对象
  */
-import {createStore, applyMiddleware} from 'redux'
+import {createStore, applyMiddleware, combineReducers} from 'redux'
 import countReducer from './reducers/count'
+import personReducer from "./reducers/person";
+
 /**
  * 引入redux-thunk 支持异步的action
  */
 import thunk from 'redux-thunk'
 
-const store = createStore(countReducer, applyMiddleware(thunk))
+// 多个reducer合并
+const allReduceers = combineReducers({person:personReducer, count: countReducer});
+
+// const store = createStore(countReducer, applyMiddleware(thunk))
+const store = createStore(allReduceers, applyMiddleware(thunk))
 export default store;
