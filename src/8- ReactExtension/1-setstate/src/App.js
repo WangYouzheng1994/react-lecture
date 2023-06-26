2b36f3ab3a82989283a58d7efbd17c9e16030173
wangyouzheng <353735348@qq.com>
2023-06-26 17:50:15 +0800

    setState init

---
+++ src/8- ReactExtension/1-setstate/src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import Count from "./components/Count";
+//引入的是容器组件
+
+class App extends Component {
+  render() {
+    return (
+        <div>
+          <Count/>
+        </div>
+    )
+  }
+}
+
+export default App;
+
